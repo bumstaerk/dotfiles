d213bd49094d55fafb64de8ff98dba1c4108aa28
Martin Knoll Haakansson <bumstaerk@DESKTOP-LM4SNUB.usg-portal>
2024-02-01 18:38:11 +0100

    Added git and ssh-agent to zsh + did a bit of waybar styling

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -3,6 +3,9 @@
 # plugins
 zplug "zsh-users/zsh-syntax-highlighting"
 zplug "zsh-users/zsh-autosuggestions"
+zplug "zsh-users/zsh-completions"
+zplug "plugins/git", from:oh-my-zsh
+zplug "plugins/ssh-agent", from:oh-my-zsh
 
 # load zplug
 zplug load 
