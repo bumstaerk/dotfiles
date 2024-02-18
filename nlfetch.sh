6669c6dc905a60153bf7f40af8e52987547f0f66
Martin Knoll Haakansson <bumstaerk@DESKTOP-LM4SNUB.usg-portal>
2024-02-18 21:02:08 +0100

    Stuff was made

---
+++ nlfetch.sh
@@ -0,0 +1,39 @@
+#!/bin/bash                                                                                          
+txtgrn='\e[0;32m' # Green
+txtylw='\e[0;33m' # Yellow
+txtblu='\e[0;34m' # Blue
+txtpur='\e[0;35m' # Purple
+txtwht='\e[0;37m' # White                                                                                         
+                                                                                          
+echo -e "         ${txtpur}.::::            ::::.           "   
+echo -e "       -+#%##%%%*        *%%%##%#+-       "     
+echo -e "     =######-.  .        .  .-######=     "     
+echo -e "   .######%                    %######.   "     
+echo -e "  :%###%##%                    %##%####.  "     
+echo -e "  ####%%###+    ${txtwht}:-::::::-:${txtpur}    +%##%%####  "     
+echo -e " =%##%%####%+ ${txtwht}.=          =.${txtpur} +%####%%##%= "     
+echo -e " ###%%%##%%##${txtwht}#*            *#${txtpur}##%####%%### "     
+echo -e " ###%%##%%%##${txtwht}#*            #${txtpur}####%%##%%##% "     
+echo -e " ###%%##%%##%%${txtwht}%=...    ...=%${txtpur}%%##%%##%%##% "     
+echo -e " *##%%#%%%#%%${txtwht}%* ${txtblu}=*=-  :=+=${txtwht} #${txtpur}%%%#%%##%%##* "     
+echo -e " -%#%%#%*=%#-${txtwht}:*:.-. --.:-.-+:${txtpur}=#%=*%#%%#%- "     
+echo -e "  *#%%#%. *:   ${txtwht}.-= .. . +:.   ${txtpur}:* .%%%%#*  "     
+echo -e "  .##%*%-        ${txtwht}-+=-=-+:        ${txtpur}-%*%##   "     
+echo -e "   .*##:-          ${txtwht}-  -          ${txtpur}-:##*.   "     
+echo -e "     =#+                          +%=     "     
+echo -e "       ==                        ==       "     
+                                                                                          
+                                                                                          
+                                                                                          
+                                                                                          
+                                                                                          
+                                                                                          
+                                                                                          
+                                                                                          
+                                                                                          
+                                                                                          
+                                                                                          
+                                                                                          
+                                                                                          
+                                                                                          
+
